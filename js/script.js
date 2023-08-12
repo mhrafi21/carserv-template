d04187d3d5dd5499138f9170d0dc1a30ddd083d4
12mhrafi <12mhrafi>
2023-08-12 16:31:22 +0600

    updated

--- js/script.js
+++ js/script.js
@@ -32,3 +32,49 @@ function pre(){
         img.src = imgItems[count];
     }
 }
+
+// hero section swipper js
+    var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      loop: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+    
+// swipper js in testimonial 
+// <!-- Initialize Swiper -->
+
+// <!-- Initialize Swiper -->
+ var swiper = new Swiper(".swipper-container", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      "@0.00": {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      "@0.75": {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      "@1.00": {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
+      "@1.50": {
+        slidesPerView: 3,
+        spaceBetween: 50,
+      },
+    },
+  });
